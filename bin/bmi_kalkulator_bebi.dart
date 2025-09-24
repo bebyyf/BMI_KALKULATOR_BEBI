10c5c80e858a5016e368d3cc92bd49d16734e115
bebyyf <bebifindia@gmail.com>
2025-09-24 18:48:31 +0700

    First Commit

---
+++ bin/bmi_kalkulator_bebi.dart
@@ -0,0 +1,5 @@
+import 'package:bmi_kalkulator_bebi/bmi_kalkulator_bebi.dart' as bmi_kalkulator_bebi;
+
+void main(List<String> arguments) {
+  print('Hello world: ${bmi_kalkulator_bebi.calculate()}!');
+}
---
+++ test/bmi_kalkulator_bebi_test.dart
@@ -0,0 +1,8 @@
+import 'package:bmi_kalkulator_bebi/bmi_kalkulator_bebi.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
